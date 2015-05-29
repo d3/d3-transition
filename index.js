29f72f538c83dfd62f2b9055f4bcc20ea00117ec
Mike Bostock <mbostock@gmail.com>
2015-05-28 21:44:56 -0700

    Remove global.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+import {selection} from "d3-selection";
 import ease from "./src/ease";
 import selection_transition from "./src/selection_transition";
 import Transition from "./src/transition";
 
-d3.selection.prototype.transition = selection_transition;
+selection.prototype.transition = selection_transition;
 
 export {
   ease,
