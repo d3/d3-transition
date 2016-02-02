239b4d59939e46c073d6a85fa50b74c30690bc41
Mike Bostock <mbostock@gmail.com>
2016-02-02 10:15:17 -0800

    Improve transition.attr.
    Support namespaced attributes. Short-circuit when the start and end values are
    equal. Remove the attribute when the end value is null.

--- src/transition/attr.js
+++ src/transition/attr.js
@@ -1,32 +1,65 @@
 import {interpolate} from "d3-interpolate";
+import {namespace} from "d3-selection";
 
-// TODO namespaced attributes
 // TODO transform interpolation
-// TODO null tween when start and end attribute values are equal
-// TODO remove attribute when end value is null
 
-function attrFunction(name, value) {
+function attrRemove(name) {
   return function() {
-    var node = this, i = interpolate(node.getAttribute(name), value.apply(this, arguments) + "");
-    return function(t) {
-      node.setAttribute(name, i(t));
-    };
+    this.removeAttribute(name);
   };
 }
 
-function attrConstant(name, value) {
-  return value += "", function() {
-    var node = this, i = interpolate(node.getAttribute(name), value);
-    return function(t) {
+function attrRemoveNS(fullname) {
+  return function() {
+    this.removeAttributeNS(fullname.space, fullname.local);
+  };
+}
+
+function attrConstant(name, value1) {
+  return value1 += "", function() {
+    var node = this, value0 = node.getAttribute(name), i;
+    return value0 !== value1 && (i = interpolate(value0, value1), function(t) {
       node.setAttribute(name, i(t));
-    };
+    });
+  };
+}
+
+function attrConstantNS(fullname, value1) {
+  return value1 += "", function() {
+    var node = this, value0 = node.getAttributeNS(fullname.space, fullname.local), i;
+    return value0 !== value1 && (i = interpolate(value0, value1), function(t) {
+      node.setAttributeNS(fullname.space, fullname.local, i(t));
+    });
+  };
+}
+
+function attrFunction(name, value) {
+  return function() {
+    var node = this, value0, value1 = value.apply(node, arguments), i;
+    if (value1 == null) return node.removeAttribute(name);
+    value0 = node.getAttribute(name), value1 += "";
+    return value0 !== value1 && (i = interpolate(value0, value1), function(t) {
+      node.setAttribute(name, i(t));
+    });
+  };
+}
+
+function attrFunctionNS(fullname, value) {
+  return function() {
+    var node = this, value0, value1 = value.apply(node, arguments), i;
+    if (value1 == null) return node.removeAttributeNS(fullname.space, fullname.local);
+    value0 = node.getAttributeNS(fullname.space, fullname.local), value1 += "";
+    return value0 !== value1 && (i = interpolate(value0, value1), function(t) {
+      node.setAttributeNS(fullname.space, fullname.local, i(t));
+    });
   };
 }
 
 export default function(name, value) {
-  return arguments.length
-      ? this.tween("attr." + name, (typeof value === "function"
-          ? attrFunction
-          : attrConstant)(name, value))
-      : this.tween("attr." + name);
+  if (arguments.length < 2) return this.tween("attr." + name);
+  var fullname = namespace(name);
+  return this.tween("attr." + name, (value == null
+      ? (fullname.local ? attrRemoveNS : attrRemove) : (typeof value === "function"
+      ? (fullname.local ? attrFunctionNS : attrFunction)
+      : (fullname.local ? attrConstantNS : attrConstant)))(fullname, value));
 }
