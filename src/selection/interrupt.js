be9bcc2304029b9e6d881fa1a4f05abc3d773f5f
Mike Bostock <mbostock@gmail.com>
2016-02-04 17:17:27 -0800

    Add selection.interrupt.

--- src/selection/interrupt.js
+++ src/selection/interrupt.js
@@ -1,5 +1,22 @@
-export default function(/* TODO name */) {
+import {namekey} from "../transition/index";
+
+export default function(name) {
+  var key = namekey(name);
   return this.each(function() {
-    // TODO
+    var schedule = this[key];
+    if (schedule) {
+      var pending = schedule.pending,
+          active = schedule.active;
+      for (var i = 0, n = pending.length; i < n; ++i) {
+        pending[i].timer.stop();
+      }
+      pending.length = 0;
+      if (active) {
+        schedule.active = null;
+        active.timer.stop();
+        active.dispatch.interrupt.call(this, this.__data__, active.index, active.group); // TODO try-catch?
+      }
+      delete this[key];
+    }
   });
 }
