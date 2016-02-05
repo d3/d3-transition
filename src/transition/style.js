14a0dab8b0b230b97418e7cf6321056d0f977050
Mike Bostock <mbostock@gmail.com>
2016-02-05 14:12:25 -0800

    Adopt d3.window from d3-selection.

--- src/transition/style.js
+++ src/transition/style.js
@@ -1,9 +1,9 @@
 import {interpolate} from "d3-interpolate";
-import defaultView from "../defaultView";
+import {window} from "d3-selection";
 
 function styleRemove(name) {
   return function() {
-    var style = defaultView(this).getComputedStyle(this, null),
+    var style = window(this).getComputedStyle(this, null),
         value0 = style.getPropertyValue(name),
         value1 = (this.style.removeProperty(name), style.getPropertyValue(name));
     if (value0 !== value1) return interpolate(value0, value1);
@@ -18,7 +18,7 @@ function styleRemoveEnd(name) {
 
 function styleConstant(name, value1) {
   return value1 += "", function() {
-    var value0 = defaultView(this).getComputedStyle(this, null).getPropertyValue(name);
+    var value0 = window(this).getComputedStyle(this, null).getPropertyValue(name);
     if (value0 !== value1) return interpolate(value0, value1);
   };
 }
@@ -27,7 +27,7 @@ function styleFunction(name, value) {
   return function() {
     var value0, value1 = value.apply(this, arguments);
     if (value1 == null) return void this.style.removeProperty(name);
-    value0 = defaultView(this).getComputedStyle(this, null).getPropertyValue(name), value1 += "";
+    value0 = window(this).getComputedStyle(this, null).getPropertyValue(name), value1 += "";
     if (value0 !== value1) return interpolate(value0, value1);
   };
 }
