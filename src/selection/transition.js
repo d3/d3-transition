cf420627f43c04199161e6c78e237b4cf0481eb8
Mike Bostock <mbostock@gmail.com>
2016-02-11 09:38:03 -0800

    Avoid copy during selection.transition.
    Since selection groups are now copy-on-write (d3/d3-selection#58), there’s no
    need to make a copy when deriving the transition from a selection!

--- src/selection/transition.js
+++ src/selection/transition.js
@@ -8,13 +8,13 @@ export default function(name) {
       key = namekey(name),
       timing = {time: now(), delay: 0, duration: 250, ease: easeCubicInOut};
 
-  for (var groups = this._groups, m = groups.length, subgroups = new Array(m), j = 0; j < m; ++j) {
-    for (var group = groups[j], n = group.length, subgroup = subgroups[j] = new Array(n), node, i = 0; i < n; ++i) {
+  for (var groups = this._groups, m = groups.length, j = 0; j < m; ++j) {
+    for (var group = groups[j], n = group.length, node, i = 0; i < n; ++i) {
       if (node = group[i]) {
-        schedule(subgroup[i] = node, key, id, i, subgroup, timing);
+        schedule(node, key, id, i, group, timing);
       }
     }
   }
 
-  return new Transition(subgroups, this._parents, key, id);
+  return new Transition(groups, this._parents, key, id);
 }
