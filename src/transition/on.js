1ffc4612871d7f0da71f61eafd15c42566124396
Mike Bostock <mbostock@gmail.com>
2016-02-26 15:26:00 -0800

    Allow some listener changes after init.
    You’re not allowed to modify start event listeners after the transition is
    scheduled, so as to avoid changes during a start event (due to changes being
    copy-on-write). However, you should still be allowed to change the OTHER event
    listeners (interrupt and end) before the transition starts; in particular,
    transition.remove should be allowed during a start event!

--- src/transition/on.js
+++ src/transition/on.js
@@ -1,10 +1,18 @@
-import {get, init} from "./schedule";
+import {get, set, init} from "./schedule";
+
+function start(name) {
+  return (name + "").trim().split(/^|\s+/).every(function(t) {
+    var i = t.indexOf(".");
+    if (i >= 0) t = t.slice(0, i);
+    return !t || t === "start";
+  });
+}
 
 function onFunction(key, id, name, listener) {
   if (typeof listener !== "function") throw new Error;
-  var on0, on1;
+  var on0, on1, sit = start(name) ? init : set;
   return function() {
-    var schedule = init(this, key, id),
+    var schedule = sit(this, key, id),
         on = schedule.on;
 
     // If this node shared a dispatch with the previous node,
