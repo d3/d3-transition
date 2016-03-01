6a8e5d08849b65afe63566c2fb1f5e1dc36b7e01
Mike Bostock <mbostock@gmail.com>
2016-03-01 14:08:51 -0800

    Simpler version export.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+export {version} from "./build/version";
 import "./src/selection/index";
 export {default as transition} from "./src/transition/index";
 export {default as active} from "./src/active";
