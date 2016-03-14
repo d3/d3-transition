f39ca248669f2123b1322303d473dd9fb88aa060
Mike Bostock <mbostock@gmail.com>
2016-03-14 12:32:50 -0700

    Windows build compatibility.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export {version} from "./build/version";
+export {version} from "./build/package";
 import "./src/selection/index";
 export {default as transition} from "./src/transition/index";
 export {default as active} from "./src/active";
