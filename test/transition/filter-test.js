33417f8b817a1a55cd9aade099902d83c2a45953
Mike Bostock <mbostock@gmail.com>
2016-06-23 16:49:59 -0700

    Change transition.filter to not preserve index.
    Related d3/d3-selection@f1c87ca14824cd42c6528cfef9e2c679ed7a1caf.

--- test/transition/filter-test.js
+++ test/transition/filter-test.js
@@ -10,7 +10,7 @@ tape("transition.filter(selector) retains the elements matching the specified se
       transition1 = d3_selection.selectAll([one, two]).data([1, 2]).transition().delay(function(d) { return d * 10; }),
       transition2 = transition1.filter("#two");
   test.equal(transition2 instanceof d3_transition.transition, true);
-  test.deepEqual(transition2._groups, [[, two]]);
+  test.deepEqual(transition2._groups, [[two]]);
   test.equal(transition2._parents, transition1._parents);
   test.equal(transition2._name, transition1._name);
   test.equal(transition2._id, transition1._id);
@@ -24,7 +24,7 @@ tape("transition.filter(function) retains the elements for which the specified f
       transition1 = d3_selection.selectAll([one, two]).data([1, 2]).transition().delay(function(d) { return d * 10; }),
       transition2 = transition1.filter(function() { return this === two; });
   test.equal(transition2 instanceof d3_transition.transition, true);
-  test.deepEqual(transition2._groups, [[, two]]);
+  test.deepEqual(transition2._groups, [[two]]);
   test.equal(transition2._parents, transition1._parents);
   test.equal(transition2._name, transition1._name);
   test.equal(transition2._id, transition1._id);
