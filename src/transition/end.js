7285f514578014f1a7630523cac88a7e5479e74a
Mike Bostock <mbostock@gmail.com>
2019-01-24 16:35:35 -0800

    Use anonymous listeners.

--- src/transition/end.js
+++ src/transition/end.js
@@ -1,9 +1,26 @@
-// TODO Anonymous names for event listeners.
+import {set} from "./schedule";
+
 export default function() {
-  var that = this, size = that.size();
+  var on0, on1, that = this, id = that._id, size = that.size();
   return new Promise(function(resolve, reject) {
-    that.on("cancel.end interrupt.end", reject).on("end.end", function() {
-      if (--size === 0) resolve();
+    var cancel = {value: reject},
+        end = {value: function() { if (--size === 0) resolve(); }};
+
+    that.each(function() {
+      var schedule = set(this, id),
+          on = schedule.on;
+
+      // If this node shared a dispatch with the previous node,
+      // just assign the updated shared dispatch and we’re done!
+      // Otherwise, copy-on-write.
+      if (on !== on0) {
+        on1 = (on0 = on).copy();
+        on1._.cancel.push(cancel);
+        on1._.interrupt.push(cancel);
+        on1._.end.push(end);
+      }
+
+      schedule.on = on1;
     });
   });
 }
